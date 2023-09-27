02525eb611712d80c89c9c19344eac0326bb6dfc
Ilhamalgojoli <rilhamgojali123@gmail.com>
2023-09-27 09:53:02 +0700

    README.md

---
+++ Jurnal.java
@@ -0,0 +1,11 @@
+public class Jurnal {
+    public static void main (String[] args)
+    {
+       
+    System.out.println ("Ilham Al Gojali");
+    System.out.println ("Ilham");
+    System.out.println ("607062300081");
+    System.out.println ("D3IF-47-04");
+
+    }
+}
